be0dd6b23b942e981d751ef2a8605220808a3619
Adam Dziomdziora <190140@edu.p.lodz.pl>
2019-05-10 10:42:23 +0200

    evaluation

--- Program.cs
+++ Program.cs
@@ -44,6 +44,27 @@ namespace Step0Regression {
 
             Console.WriteLine ($"Predicted price for size: {size.Size * 1000} sq ft= {price.Price * 100:C}");
 
+            // 5. Import or create test data          
+            HouseData[] testHouseData = {
+                new HouseData () { Size = 1.1F, Price = 0.98F },
+                new HouseData () { Size = 1.9F, Price = 2.1F },
+                new HouseData () { Size = 2.8F, Price = 2.9F },
+                new HouseData () { Size = 3.4F, Price = 3.6F }
+            };
+            IDataView testDataView = mlContext.Data.LoadFromEnumerable (houseData);
+            //
+
+            // 6. Use trained model to predict values
+            var testPriceDataView = model.Transform (testDataView);
+
+            // 6.A Preview predicted data in the debugger
+            var testPriceDataViewDebugger = testPriceDataView.Preview();
+
+
+            // 7. Evaluate test data 
+            var metrics = mlContext.Regression.Evaluate (testPriceDataView, labelColumnName: "Price");
+            Console.WriteLine ($"R^2: {metrics.RSquared:0.##}");
+            Console.WriteLine ($"RMS error: {metrics.RootMeanSquaredError:0.##}");
         }
     }
 }
