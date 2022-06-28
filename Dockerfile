463a8ba7e77e247e5a380a50d6dfd7de1dafbae1
CYBERDOM2\addzi1 <adam.dziomdziora@cybercom.com>
2022-06-28 14:53:20 +0200

    update to net 6

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+#See https://aka.ms/containerfastmode to understand how Visual Studio uses this Dockerfile to build your images for faster debugging.
+
+FROM mcr.microsoft.com/dotnet/runtime:6.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["Step0Regression.csproj", "."]
+RUN dotnet restore "./Step0Regression.csproj"
+COPY . .
+WORKDIR "/src/."
+RUN dotnet build "Step0Regression.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "Step0Regression.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "Step0Regression.dll"]
--- Program.cs
+++ Program.cs
@@ -1,70 +1,63 @@
-﻿using System;
+﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.ML;
 using Step0Regression.Models;
 
-namespace Step0Regression {
-    class Program {
+Console.WriteLine("Hello, World!");
 
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello World!");
+// 0. Create the common context for all ML.NET operations
+var mlContext = new MLContext();
+//
 
-            // 0. Create the common context for all ML.NET operations
-            var mlContext = new MLContext ();
-            //
-
-            // 1. Import or create training data          
-            HouseData[] houseData = new [] {
+// 1. Import or create training data          
+var houseData = new[] {
                 new HouseData () { Size = 1.1F, Price = 1.2F },
                 new HouseData () { Size = 1.9F, Price = 2.3F },
                 new HouseData () { Size = 2.8F, Price = 3.0F },
                 new HouseData () { Size = 3.4F, Price = 3.7F }
             };
-            IDataView trainingData = mlContext.Data.LoadFromEnumerable (houseData);
-            //
+IDataView trainingData = mlContext.Data.LoadFromEnumerable(houseData);
+//
 
-            // 1A. Preview loaded data in the debugger
-            var trainingDataDebugger = trainingData.Preview ();
-            //
+// 1A. Preview loaded data in the debugger
+var trainingDataDebugger = trainingData.Preview();
+//
 
-            // 2. Specify data preparation and model training pipeline
-            var pipeline = mlContext.Transforms
-                .Concatenate ("Features", new [] { "Size" })
-                .Append (mlContext.Regression.Trainers.Sdca (labelColumnName: "Price", maximumNumberOfIterations : 100));
-            //
+// 2. Specify data preparation and model training pipeline
+var pipeline = mlContext.Transforms
+    .Concatenate("Features", new[] { "Size" })
+    .Append(mlContext.Regression.Trainers.Sdca(labelColumnName: "Price", maximumNumberOfIterations: 100));
+//
 
-            // 3. Train model
-            var model = pipeline.Fit (trainingData);
-            //
+// 3. Train model
+var model = pipeline.Fit(trainingData);
+//
 
-            // 4. Make a prediction
-            var size = new HouseData () { Size = 2.5F };
-            var predictionEngine = mlContext.Model.CreatePredictionEngine<HouseData, Prediction> (model);
-            var price = predictionEngine.Predict (size);
-            //
+// 4. Make a prediction
+var size = new HouseData() { Size = 2.5F };
+var predictionEngine = mlContext.Model.CreatePredictionEngine<HouseData, Prediction>(model);
+var price = predictionEngine.Predict(size);
+//
 
-            Console.WriteLine ($"Predicted price for size: {size.Size * 1000} sq ft= {price.Price * 100:C}");
+Console.WriteLine($"Predicted price for size: {size.Size * 1000} sq ft= {price.Price * 100:C}");
 
-            // 5. Import or create test data          
-            HouseData[] testHouseData = {
+// 5. Import or create test data          
+var testHouseData = new[]{
                 new HouseData () { Size = 1.1F, Price = 0.98F },
                 new HouseData () { Size = 1.9F, Price = 2.1F },
                 new HouseData () { Size = 2.8F, Price = 2.9F },
                 new HouseData () { Size = 3.4F, Price = 3.6F }
             };
-            IDataView testDataView = mlContext.Data.LoadFromEnumerable (houseData);
-            //
+IDataView testDataView = mlContext.Data.LoadFromEnumerable(houseData);
+//
 
-            // 6. Use trained model to predict values
-            var testPriceDataView = model.Transform (testDataView);
+// 6. Use trained model to predict values
+var testPriceDataView = model.Transform(testDataView);
 
-            // 6.A Preview predicted data in the debugger
-            var testPriceDataViewDebugger = testPriceDataView.Preview();
+// 6.A Preview predicted data in the debugger
+var testPriceDataViewDebugger = testPriceDataView.Preview();
 
 
-            // 7. Evaluate test data 
-            var metrics = mlContext.Regression.Evaluate (testPriceDataView, labelColumnName: "Price");
-            Console.WriteLine ($"R^2: {metrics.RSquared:0.##}");
-            Console.WriteLine ($"RMS error: {metrics.RootMeanSquaredError:0.##}");
-        }
-    }
-}
+// 7. Evaluate test data 
+var metrics = mlContext.Regression.Evaluate(testPriceDataView, labelColumnName: "Price");
+Console.WriteLine($"R^2: {metrics.RSquared:0.##}");
+Console.WriteLine($"RMS error: {metrics.RootMeanSquaredError:0.##}");
